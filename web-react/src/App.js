dbd70258433d7668339919f7957a3bbab7983d28
Lyndon Panton <LyndonPanton@gmail.com>
2024-10-07 17:51:46 +0100

    Added content to the application header

--- web-react/src/App.js
+++ web-react/src/App.js
@@ -2,24 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <header>
+            <Navigation>
+
+            </Navigation>
+
+            <h1>Todo List</h1>
+        </header>
+    )
+}
+
+function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About Me</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default App;
