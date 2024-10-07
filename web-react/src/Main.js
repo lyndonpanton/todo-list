041c0f287f5f22f32cd2ae80001388728421c3e6
Lyndon Panton <LyndonPanton@gmail.com>
2024-10-07 18:03:50 +0100

    Added todo list component

--- web-react/src/Main.js
+++ web-react/src/Main.js
@@ -1,9 +1,22 @@
 function Main() {
     return (
         <main>
-            Main content here...
+            <Todo />
         </main>
-    )
+    );
+}
+
+function Todo() {
+    return (
+        <article>
+            <h2>My Todo List{ /* Editable list name*/ }</h2>
+            <ul id="todo-list">
+                <li>Buy groceries</li>
+                <li>Feed cat</li>
+                <li>Email Bob</li>
+            </ul>
+        </article>
+    );
 }
 
 export default Main;
