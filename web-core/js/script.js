9a363fc3e2eb829ef23adaf48310486a2f538391
Lyndon Panton <LyndonPanton@gmail.com>
2024-10-12 21:31:02 +0100

    New todos can be added to the list

--- web-core/js/script.js
+++ web-core/js/script.js
@@ -1,11 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-   let todoCheckboxes = document.getElementsByClassName("todo-list-checkbox");
+    let todoList = document.getElementById("todo-list");
+    let todoCheckboxes = document.getElementsByClassName("todo-list-checkbox");
 
-   for (let i = 0; i < todoCheckboxes.length; i++) {
-       todoCheckboxes[i].addEventListener("click", checkTodo);
-   }
+    for (let i = 0; i < todoCheckboxes.length; i++) {
+        todoCheckboxes[i].addEventListener("click", checkTodo);
+    }
 
-   function checkTodo(event) {
+    let addTodoInput = document.getElementById("add-todo-input");
+    let addTodoButton = document.getElementById("add-todo-button");
+    addTodoButton.addEventListener("click", addTodo);
+
+    let clearTodoListButton = document.getElementById("clear-todo-list-button");
+
+    function addTodo(event) {
+        event.preventDefault();
+
+        let todoText = document.createTextNode(addTodoInput.value);
+        console.log(addTodoInput.value);
+        let todoItem = document.createElement("li");
+        todoItem.classList.add("todo-list-item");
+
+        todoItem.append(todoText);
+        todoList.append(todoItem);
+    }
+
+    function checkTodo(event) {
        event.target.parentElement.classList.toggle("todo-list-item-complete");
-   }
+    }
 });
