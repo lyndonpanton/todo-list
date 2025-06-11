e9041bf1f57382ec105cf961c67f693ba7e1672d
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-11 18:26:21 +0100

    Configured webpack to work with images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles.css";
 
+const image = document.createElement("img");
+image.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Aptenodytes_forsteri_-Snow_Hill_Island%2C_Antarctica_-adults_and_juvenile-8.jpg/250px-Aptenodytes_forsteri_-Snow_Hill_Island%2C_Antarctica_-adults_and_juvenile-8.jpg";
+
+document.body.appendChild(image);
+
 console.log("Webpack has been configured successfully");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -20,6 +19,14 @@ module.exports = {
             {
                 test: /\.css$/i,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource"
             }
         ]
     }
