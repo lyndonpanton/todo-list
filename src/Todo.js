63a8ac005c710ceeb09746a9c3f5d05f123f8c01
Lyndon Panton <LyndonPanton@gmail.com>
2025-06-11 19:34:26 +0100

    Create Todo class

---
+++ src/Todo.js
@@ -0,0 +1,43 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.id = self.crypto.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+}
+
+export default Todo;
